a5d926871b2cac0fbd02001447981a81a8cff7dd
wissemhjm <129502701+wissemhjm@users.noreply.github.com>
2023-05-16 22:52:47 +0100

    your message

--- oop/oop.js
+++ oop/oop.js
@@ -7,7 +7,7 @@
 // Salary (a float)
 // The Employee class should have the following methods:
 
-// display_info(): Prints the name, employee ID, department, and salary of the employee.
+// display_inf(): Prints the name, employee ID, department, and salary of the employee.
 // Create a class called EmployeeManagementSystem. Each EmployeeManagementSystem object should have the following properties:
 
 // Company name (a string)
@@ -18,3 +18,26 @@
 // fire_employee(employee_id): Removes the employee with the given ID from the list of employees.
 // display_employees(): Prints the information of all the employees in the system.
 // Create a few instances of the Employee class and add them to an instance of the EmployeeManagementSystem class. Test out the methods to hire and fire employees, and display the employee information.
+
+
+function Employee(employeeID, department, salary) {
+    employeeID : employeeID,
+    department : department,
+    salary : salary,
+  
+    display_inf = function() {
+      this.employeeID,
+      this.department,
+      this.salary
+    };
+  }
+  
+  function EmployeeManagementSystem(company) {
+    company = company;
+    listOfEmployees = [];
+  
+    hire_employee = function(employee) {
+      this.listOfEmployees.push(employee);
+      return employee.employeeID
+    };
+}
